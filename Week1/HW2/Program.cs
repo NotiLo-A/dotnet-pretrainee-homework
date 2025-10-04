7a66e853cd1c630fee2b12cab61a5c0b0837940a
NotiLo-A <ninedefix@proton.me>
2025-10-04 17:41:44 +0300

    Add Week01 HW2

---
+++ Week1/HW2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HW2;
+
+class Program
+{
+    static async Task Main()
+    {
+        var files = new[] { "file 1", "file 2", "file 3" };
+        var tasks = files.Select(ProcessDataAsync);
+        
+        var results = await Task.WhenAll(tasks);
+
+        foreach (var result in results)
+        {
+            Console.WriteLine(result);
+        }
+    }
+    
+    static async Task<string> ProcessDataAsync(string dataName)
+    {
+        await Task.Delay(3000);
+        return $"Processing '{dataName}' completed in 3 seconds";
+    }
+}
